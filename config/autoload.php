a49161706f2360023abd7ac0d658074ce97de5c5
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2013-04-04 14:30:40 +0200

    initial

---
+++ config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package Trennlinie
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'Trennlinie' => 'system/modules/trennlinie/Trennlinie.php',
+));
---
+++ templates/ce_trennlinie.xhtml
@@ -0,0 +1,1 @@
+<div class="ce_trennlinie"><hr /></div>
