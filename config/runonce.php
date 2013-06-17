b6174430af1f38984e257501224a1b6bf0ccd257
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2013-06-17 15:41:07 +0200

    initial 2

--- config/runonce.php
+++ config/runonce.php
@@ -1,18 +1,18 @@
 <?php
 class DividerRunonceJob extends Controller
 {
-   public function __construct()
-   {
-       parent::__construct();
-   }
-   public function run()
-   {
-	   $this->import('Files');
-	   if ((@include(TL_ROOT . '/system/modules/Divider')) !== false)
-	   {
-		   $this->Files->rrdir(TL_ROOT . 'system/modules/Divider');
-	   }
-   }
+public function __construct()
+{
+	parent::__construct();
+}
+public function run()
+{
+	$this->import('Files');
+
+	if ((@include(TL_ROOT . '/system/modules/Divider')) !== false) 
+	{
+		$this->Files->rrdir(TL_ROOT . 'system/modules/Divider');
+	}
 }
 $objDividerRunonceJob = new DividerRunonceJob();
 $objDividerRunonceJob->run();
