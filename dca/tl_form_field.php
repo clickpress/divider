e3fa48d7e67d10fbba5a12f2c7239d53963d7b18
Stefan SL <ssl@clickpress.de>
2016-10-20 12:05:47 +0200

    Add custom tpl option

--- dca/tl_form_field.php
+++ dca/tl_form_field.php
@@ -12,6 +12,6 @@
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_form_field']['palettes']['divider'] = '{type_legend},type';
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['divider'] = '{type_legend},type;{template_legend:hide},customTpl';
 
 ?>
